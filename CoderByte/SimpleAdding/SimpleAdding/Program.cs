bdabc55b90aa70bd81586732cc206179e9337840
Martin Rodin <rodin.martin@gmail.com>
2016-10-04 12:40:37 +0200

    add SimpleAdding solution

---
+++ CoderByte/SimpleAdding/SimpleAdding/Program.cs
@@ -0,0 +1,24 @@
+﻿// Using the C# language, have the function SimpleAdding(num) add up all the numbers
+// from 1 to num. For the test cases, the parameter num will be any number from 1 to 1000.
+
+using System;
+
+namespace SimpleAdding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SimpleAdding(12));
+            Console.WriteLine(SimpleAdding(140));
+            Console.ReadKey();
+        }
+
+        public static int SimpleAdding(int num)
+        {
+            if (num <= 1)
+                return 1;
+            return num + SimpleAdding(num - 1);
+        }
+    }
+}
