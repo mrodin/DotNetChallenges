d36e85b345755e4bc3fe5593217d2e6150c4af32
Martin Rodin <rodin.martin@gmail.com>
2016-10-07 14:47:07 +0200

    add TimeConvert solution

---
+++ CoderByte/TimeConvert/TimeConvert/Program.cs
@@ -0,0 +1,26 @@
+﻿// Using the C# language, have the function TimeConvert(num) take the num parameter being passed
+// and return the number of hours and minutes the parameter converts to (ie. if num = 63 then
+// the output should be 1:3). Separate the number of hours and minutes with a colon.
+
+using System;
+
+namespace TimeConvert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(TimeConvert(126));
+            Console.WriteLine(TimeConvert(45));
+            Console.ReadKey();
+        }
+
+        public static string TimeConvert(int num)
+        {
+            int hours = num / 60;
+            int minutes = num % 60;
+
+            return hours + ":" + minutes;
+        }
+    }
+}
