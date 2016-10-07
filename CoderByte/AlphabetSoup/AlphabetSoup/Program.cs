b947f1bcb96a13246d71b2221763aa6c0a64861f
Martin Rodin <rodin.martin@gmail.com>
2016-10-07 15:42:23 +0200

    add AlphabetSoup solution

---
+++ CoderByte/AlphabetSoup/AlphabetSoup/Program.cs
@@ -0,0 +1,27 @@
+﻿// Using the C# language, have the function AlphabetSoup(str) take the str string parameter being passed
+// and return the string with the letters in alphabetical order (ie. hello becomes ehllo). Assume numbers
+// and punctuation symbols will not be included in the string.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlphabetSoup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(AlphabetSoup("coderbyte"));
+            Console.WriteLine(AlphabetSoup("hooplah"));
+            Console.ReadKey();
+        }
+
+        public static string AlphabetSoup(string str)
+        {
+            return string.Concat(str.OrderBy(c => c));
+        }
+    }
+}
