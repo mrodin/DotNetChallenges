e4a0daceb43c7ead501b078a07b87ae5687804b6
Martin Rodin <rodin.martin@gmail.com>
2016-10-11 14:17:28 +0200

    add VeryBigSum solution

---
+++ HackerRank/VeryBigSum/VeryBigSum/Program.cs
@@ -0,0 +1,26 @@
+﻿// https://www.hackerrank.com/challenges/a-very-big-sum
+
+using System;
+
+namespace VeryBigSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1000000001, 1000000002, 1000000003, 1000000004, 1000000005 };
+            Console.WriteLine(VeryBigSum(numbers));
+            Console.ReadKey();
+        }
+
+        static long VeryBigSum(int[] numbers)
+        {
+            long result = 0;
+
+            foreach (var number in numbers)
+                result += number;
+
+            return result;
+        }
+    }
+}
