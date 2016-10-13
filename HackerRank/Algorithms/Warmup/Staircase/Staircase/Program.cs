78e8b3ea648de247039a838f3c3232da185663fd
Martin Rodin <rodin.martin@gmail.com>
2016-10-13 16:25:15 +0200

    add Staircase solution

---
+++ HackerRank/Algorithms/Warmup/Staircase/Staircase/Program.cs
@@ -0,0 +1,32 @@
+﻿// https://www.hackerrank.com/challenges/staircase
+
+using System;
+
+namespace Staircase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Staircase(6);
+            Console.ReadKey();
+        }
+
+        static void Staircase(int n)
+        {
+            int spaceCount = n;
+            int hashCount = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                spaceCount--;
+                hashCount++;
+
+                string spaces = new string(' ', spaceCount);
+                string hashes = new string('#', hashCount);
+                string line = spaces + hashes;
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
