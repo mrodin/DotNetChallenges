6f26c4af0646d84780f52522c4274f272a297fd9
Martin Rodin <rodin.martin@gmail.com>
2016-10-14 12:47:10 +0200

    add TimeConversion solution

---
+++ HackerRank/Algorithms/Warmup/TimeConversion/TimeConversion/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(TimeConversion("07:05:45PM"));
+            Console.ReadKey();
+        }
+
+        static string TimeConversion(string time)
+        {
+            DateTime dt = DateTime.Parse(time);
+            return dt.ToString("HH:mm:ss");
+        }
+    }
+}
